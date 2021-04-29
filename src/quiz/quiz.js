fd3df8357a199cf25f82b09cd7b31c112c72a68f
Kimberley Kelly <kimberley.b.kelly@gmail.com>
2021-04-29 20:40:42 +0100

    template to get you started with the tutorial

---
+++ src/quiz/quiz.js
@@ -0,0 +1,9 @@
+const Quiz = () => {
+  return (
+    <div className="quiz-container">
+      <div className="quiz-title">The Modern Family Quiz!</div>
+    </div>
+  )
+}
+
+export default Quiz
---
+++ src/quiz/results.js
@@ -0,0 +1,9 @@
+const Results = () => {
+  return (
+    <div className="results-container">
+      <div className="results-title">Results!</div>
+    </div>
+  )
+}
+
+export default Results
